737038ee245dd56e9afc82933dfa769bf8496acb
danilojsdev <danylosilva@yahoo.com.br>
2023-01-25 20:36:05 -0300

    Resolução dos exercícios da aula React-Context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import GlobalState from "./contexts/GlobalState";
+
 import Router from "./routes/Router";
 
 export default function App() {
   return (
-    <>
+    <GlobalState>
       <Router />
-    </>
+    </GlobalState>
   );
 }
